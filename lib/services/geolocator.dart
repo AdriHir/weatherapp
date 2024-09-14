21e1567687247d8362b4226c0ba7362c543da443
AdriHir <hirtzadrien@gmail.com>
2024-09-14 16:03:48 +0200

    add geolocator

---
+++ lib/services/geolocator.dart
@@ -0,0 +1,17 @@
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> getLocation() async {
+  LocationPermission permission;
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Permission de geolocalisation refusé');
+    }
+  }
+  if(permission == LocationPermission.deniedForever){
+    return Future.error("Nous ne pouvons pas acceder à la géolocalisation car celle ci est toujorus refusé");
+  }
+  return await Geolocator.getCurrentPosition();
+}
