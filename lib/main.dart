4f8704bdaadf9a99c57c268ac6cf16a175db9c58
AdriHir <hirtzadrien@gmail.com>
2024-09-17 18:57:22 +0200

    Remove row add spacer()

--- lib/main.dart
+++ lib/main.dart
@@ -64,76 +64,70 @@ class _MainAppState extends State<MainApp> {
               image: DecorationImage(
                   image: AssetImage("lib/assets/pictures/back.jpg"),
                   fit: BoxFit.fill)),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "$cityName $cp",
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SvgPicture.asset(
-                    "lib/assets/icons/animated/rainy-1.svg",
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
+          child: Column(children: [
+            Spacer(),
+            Text(
+              "$cityName $cp",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
               ),
-              Text(
-                "$temperatureInfo",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
+            ),
+            SvgPicture.asset(
+              "lib/assets/icons/animated/rainy-1.svg",
+              width: 100,
+              height: 100,
+            ),
+            Text(
+              "$temperatureInfo",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
               ),
-              SizedBox(height: 10),
-              Text(
-                "$today",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "$today",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
               ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    padding: EdgeInsets.all(16),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: weatherInfoList.map((weather) {
-                          return Container(
-                            margin: EdgeInsets.only(right: 15),
-                            // met un margin entre les boitesa droite
-                            padding: EdgeInsets.all(20),
-                            //met un paddind autour du text
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0),
-                              // rajoute une opacité<1
-                              borderRadius: BorderRadius.circular(
-                                  20), //arrondi les 4 angles
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: weatherInfoList.map((weather) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 15),
+                          // met un margin entre les boitesa droite
+                          padding: EdgeInsets.all(20),
+                          //met un paddind autour du text
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0),
+                            // rajoute une opacité<1
+                            borderRadius: BorderRadius.circular(
+                                20), //arrondi les 4 angles
+                          ),
+                          child: Text(
+                            weather,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
                             ),
-                            child: Text(
-                              weather,
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
+                          ),
+                        );
+                      }).toList(),
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
