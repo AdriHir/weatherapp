eb117725a499efa9b2e2ddfb42631173b8ffb9f7
AdriHir <hirtzadrien@gmail.com>
2024-09-19 20:23:33 +0200

    graph ok  2nd yaxis on progress

--- lib/components/chart_component.dart
+++ lib/components/chart_component.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
-
 
 class BarChartComponent extends StatefulWidget {
   const BarChartComponent({super.key});
@@ -11,35 +9,86 @@ class BarChartComponent extends StatefulWidget {
 }
 
 class BarChartComponentState extends State<BarChartComponent> {
-  List<_DayWeek> data = [
-    _DayWeek('Lun', 35),
-    _DayWeek('Mar', 28),
-    _DayWeek('Mer', 34),
-    _DayWeek('Jeu', 32),
-    _DayWeek('Ven', 40),
-    _DayWeek('Sam', 40),
-    _DayWeek('Dim', 40),
+  List<_DayWeek> temperatures = [
+    _DayWeek('Lun', 15),
+    _DayWeek('Mar', -10),
+    _DayWeek('Mer', 35),
+    _DayWeek('Jeu', -15),
+    _DayWeek('Ven', 25),
+    _DayWeek('Sam', 18),
+    _DayWeek('Dim', 7),
+  ];
+
+  List<_rainning> precipitation = [
+    _rainning('Lun', 2),
+    _rainning('Mar', 25),
+    _rainning('Mer', 30),
+    _rainning('Jeu', 46),
+    _rainning('Ven', 15),
+    _rainning('Sam', 18),
+    _rainning('Dim', 7),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        children: [
-          SfCartesianChart(
-            primaryXAxis:CategoryAxis(title: AxisTitle(text: "test"),
-              name: "°C",
+      body: Center(
+        child: SfCartesianChart(
+          primaryXAxis: CategoryAxis(
+
+            majorGridLines: MajorGridLines(width: 0),
+          ),
+          primaryYAxis: NumericAxis(
+            axisLine: AxisLine(width: 0),
+            majorTickLines: MajorTickLines(size: 0),
+            title: AxisTitle(text:
+            'Valeurs'),
+          ),
+          legend: Legend(isVisible: true),
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: <CartesianSeries>[
+            ColumnSeries<_DayWeek, String>(
+              dataSource: temperatures,
+              xValueMapper: (_DayWeek day, _) => day.week,
+              yValueMapper: (_DayWeek day, _) => day.jours,
+              name: 'Température (°C)',
+              color: Colors.red,
+              dataLabelSettings: DataLabelSettings(isVisible: true),
             ),
-          )
-        ],
+            LineSeries<_rainning, String>(
+              dataSource: precipitation,
+              xValueMapper: (_rainning time, _) => time.jour,
+              yValueMapper: (_rainning time, _) => time.preci,
+              name: "Précipitations (mm)",
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class _DayWeek {
-  _DayWeek(this.year, this.sales);
+class _rainning {
+  _rainning(this.jour, this.preci);
 
-  final String year;
-  final double sales;
+  final String jour;
+  final double preci;
+}
+
+class _DayWeek {
+  _DayWeek(this.week, this.jours);
+
+  final String week;
+  final double jours;
+}
+
+Color getColor(double temperature) {
+  if (temperature <= 10) {
+    return Colors.blue;
+  } else if (temperature <= 25) {
+    return Colors.green;
+  } else {
+    return Colors.red;
+  }
 }
